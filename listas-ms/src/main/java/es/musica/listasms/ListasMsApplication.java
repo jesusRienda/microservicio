c71ad42a9b255d0c56a7df4308d094f73dcee71c
DESKTOP-NVN5KQC\Jesus <100293417@alumnos.uc3m.es>
2019-02-22 20:59:13 +0100

    initial commit

---
+++ listas-ms/src/main/java/es/musica/listasms/ListasMsApplication.java
@@ -0,0 +1,13 @@
+package es.musica.listasms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListasMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListasMsApplication.class, args);
+	}
+
+}
